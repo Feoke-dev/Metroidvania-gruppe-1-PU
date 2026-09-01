e64b6a9adc13a45da6542a7034bd47c2106a7f33
barrythesquidd <uuser69420123@gmail.com>
2026-09-01 15:00:09 +0200

    added timer

--- Assets/_Scripts/ScoreSystem.cs
+++ Assets/_Scripts/ScoreSystem.cs
@@ -1,44 +1,87 @@
 using UnityEngine;
+using TMPro;
 
 public class ScoreSystem : MonoBehaviour
 {
     public static ScoreSystem instance;
+
     private int score = 0;
     private int totalPoints = 0;
-    TMPro.TMP_Text scoreText;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    // Elapsed time in seconds (fractional)
+    private float elapsedTime = 0f;
+
+    // Assign these in the Inspector (or leave one null to auto-find on the same GameObject)
+    public TMP_Text scoreText;
+    public TMP_Text timerText;
+
     void Start()
     {
-        if (instance != null) {
+        if (instance != null)
+        {
             Destroy(this);
             return;
         }
+
         instance = this;
-        scoreText = GetComponent<TMPro.TMP_Text>();
+
+        if (scoreText == null)
+            scoreText = GetComponent<TMP_Text>();
+
         ResetScore();
-//        scoreText.text = "0";
     }
 
-    public void ResetScore() {
+    public void ResetScore()
+    {
         score = 0;
-        scoreText.text = score.ToString();
+        if (scoreText != null)
+            scoreText.text = score.ToString();
+
         totalPoints = GameObject.FindGameObjectsWithTag("Point").Length;
+        elapsedTime = 0f;
+        UpdateTimerText();
     }
 
-    public void AddScore(int points) {
+    public void AddScore(int points)
+    {
         score += points;
-        scoreText.text = score.ToString();
+        if (scoreText != null)
+            scoreText.text = score.ToString();
     }
 
-    public int GetScore() {
+    public int GetScore()
+    {
         return score;
     }
 
-    public float GetScorePercentage() {
-        if (totalPoints == 0) {
+    public float GetScorePercentage()
+    {
+        if (totalPoints == 0)
             return 0f;
-        }
         return (float)score / totalPoints;
     }
 
+    // Set timer (seconds)
+    public void SetTimer(int time)
+    {
+        elapsedTime = time;
+        UpdateTimerText();
+    }
+
+    void Update()
+    {
+        // Advance timer each frame
+        elapsedTime += Time.deltaTime;
+        UpdateTimerText();
+    }
+
+    private void UpdateTimerText()
+    {
+        if (timerText == null)
+            return;
+
+        int seconds = (int)elapsedTime;
+        int milliseconds = (int)((elapsedTime - seconds) * 1000f);
+        timerText.text = $"{seconds}s {milliseconds:000}ms";
+    }
 }
